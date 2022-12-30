1e53027fe2b7e131ced52d7b204dcf765f7081ae
vzolt <vladimir@postbag.ru>
2022-12-30 08:33:17 +0300

    modify project

--- Final Project/crontab/cron.py
+++ Final Project/crontab/cron.py
@@ -5,7 +5,13 @@ from crontab import CronTab
 
 cron = CronTab(user= True)
 
-job = cron.new(command='python ../src/add.csv')
+job1 = cron.new(command='python ../src/add.py')
+job1.day.on(0, 1, 2, 3, 4, 5, 6)
 
-job.hour.every(24)
+job2 = cron.new(command='python ../src/transform.py')
+job2.day.on(0, 1, 2, 3, 4, 5, 6)
+
+job3 = cron.new(command='python ../src/count.py')
+job3.day.on(0, 1, 2, 3, 4, 5, 6)
+
 cron.write()
